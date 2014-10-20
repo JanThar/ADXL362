61800bed779e24cb87c29c97e5dc252713fb7bdf
Jan <jan.thar@rwth-aachen.de>
2014-10-20 18:55:43 +0200

    Aaargh...nothing worked -> manual import

--- firmware/examples/Spark_Buttons_ADXL362test.cpp
+++ firmware/examples/Spark_Buttons_ADXL362test.cpp
@@ -1,11 +1,13 @@
 //First rought testsketch for the Spark Buttons shield
 
+//!!!! For using the WS2812-ring on the Spark button the neopixel library
+// has to be manually added - the #include statement is not enough. Sorry.
+
 // This #include statement was automatically added by the Spark IDE.
-#include "neopixel/neopixel.h"
+//#include "neopixel/neopixel.h"
 #include "ADXL362/ADXL362.h"
 #include "application.h"
 
-// IMPORTANT: Set pixel COUNT, PIN and TYPE
 #define PIXEL_PIN A7
 #define PIXEL_COUNT 11
 #define PIXEL_TYPE WS2812B
@@ -18,18 +20,17 @@ int XValue, YValue, ZValue, Temperature;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
 void setup() {
-    xl.begin(A2);                   // Setup SPI protocol, issue device soft reset
-    xl.beginMeasure();              // Switch ADXL362 to measure mode  
+    xl.begin(A2);              
+    xl.beginMeasure();             
     
     strip.begin();
-    strip.show(); // Initialize all pixels to 'off'
+    strip.show(); 
 }
 
 void loop() {
     xl.readXYZTData(XValue, YValue, ZValue, Temperature);
     for(int i=0; i<strip.numPixels(); i++) {
      strip.setPixelColor(i, (char)(XValue/0xFF), (char)(YValue/0xFF), (char)(ZValue/0xFF));
-     //strip.setPixelColor(i, 0xFF, 0xFF, 0xFF);
     }
     strip.show();
     delay(100);     
